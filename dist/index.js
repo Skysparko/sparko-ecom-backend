86edc30362ff9b28ba5d06772b966441a0a694da
Shubham Rakhecha <49132205+Skysparko@users.noreply.github.com>
2023-05-16 15:00:21 +0530

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -26,6 +26,7 @@ app.use((0, morgan_1.default)("dev"));
 app.use((0, cors_1.default)({
     origin: process.env.CLIENT_APP_URL,
     credentials: true,
+    exposedHeaders: ["Set-Cookie"],
 }));
 app.use((0, cookie_parser_1.default)());
 //routes
