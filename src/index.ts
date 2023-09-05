1084d01d8a54faa65e6a128da299922b9a8b594a
Shubham Rakhecha <49132205+Skysparko@users.noreply.github.com>
2023-09-05 08:47:37 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -31,6 +31,11 @@ app.use(
 exposedHeaders: ["Set-Cookie"],
   })
 );
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 app.use(cookieParser());
 
 //routes
